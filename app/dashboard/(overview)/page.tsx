5e29fff51b4e7046e3bfa854829f1c7181a3e205
Matt Halden <just.m.halden@gmail.com>
2025-01-07 10:43:01 -0700

    streaming for cards

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,24 +2,31 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-// import query function
-import { fetchCardData } from '@/app/lib/data';
+// import query functions, removed after we streamed/suspensed data in components
+// import { fetchCardData } from '@/app/lib/data';
 // import suspense for streaming data fetch timing
 import { Suspense } from 'react';
-// import Revenue chart skeleton to show better UI for loading
-import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+// import skeletons to show better UI for loading
+import CardWrapper from '@/app/ui/dashboard/cards';
+import { 
+  RevenueChartSkeleton, 
+  LatestInvoicesSkeleton,
+  CardsSkeleton,
+ } from '@/app/ui/skeletons';
 
  
 export default async function Page() {
-  // grab data here, streaming fetchRevenue to optimize load time with <Suspense> below
+  // comment out getting data here to streaming fetchRevenue, fetchLatestInvoices, in components 
+  // to optimize load time with <Suspense> below
+
   // const revenue = await fetchRevenue();
   // const latestInvoices = await fetchLatestInvoices();
-  const { 
-    totalPaidInvoices,
-    totalPendingInvoices, 
-    numberOfInvoices,
-    numberOfCustomers,
-   } = await fetchCardData();
+  // const { 
+  //   totalPaidInvoices,
+  //   totalPendingInvoices, 
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  //  } = await fetchCardData();
 
   return (
     <main>
@@ -27,14 +34,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        /> */}
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/* Suspense here for streaming/data fetching*/}
