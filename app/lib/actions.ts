04323826e2e130c08631abff1c1ac6e988d9e9c2
Matt Halden <just.m.halden@gmail.com>
2025-01-10 11:05:16 -0700

    adjusting actions.ts delete invoice to return void

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -100,9 +100,10 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
+    //return { message: 'Deleted Invoice.' };
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
+    // return { message: 'Database Error: Failed to Delete Invoice.' };
+    throw new Error('Database Error: Failed to Delete Invoice.');
   }
   // No need to call redirect since action is getting called on /dashboard/invoices path
   // revalidatePath will trigger a new server request and re-render table
